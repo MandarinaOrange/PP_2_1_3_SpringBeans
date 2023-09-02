04d9ad2008197a568ae2ba13e38386f60aeaf324
Mandarin <msdolphin@yandex.ru>
2023-09-02 18:50:07 +0300

    MM

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,8 +1,10 @@
 package app.model;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("Dog")
+@Primary //либо без примари и с qualifier
 public class Dog extends Animal {
     @Override
     public String toString() {return "I'm a Dog";}
